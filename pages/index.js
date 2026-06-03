1540b5a7ac6c96171c27f8a9910e2767a12d5977
Ayush Sharma <ayush.sharmaa@hotmail.com>
2026-06-03 20:06:47 +0530

    Commit

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,36 @@
+import Head from 'next/head'
+import { useState, useEffect } from 'react'
+import Navbar from '../components/Navbar'
+import Hero from '../components/Hero'
+import Steps from '../components/Steps'
+import Tasks from '../components/Tasks'
+import Footer from '../components/Footer'
 
+export default function Home() {
+  const [isScrolled, setIsScrolled] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      setIsScrolled(window.scrollY > 50)
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+
+  return (
+    <>
+      <Head>
+        <title>Campus Mantri - GeeksforGeeks</title>
+        <meta name="description" content="Join the GeeksforGeeks Campus Mantri Program and grow your network" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+      <Hero />
+      <Steps />
+      <Tasks />
+      <Footer />
+    </>
+  )
+}
